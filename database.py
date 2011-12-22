147b8c3ea94818bb182993c15c1cfe319d648933
Patrick Rauscher <prauscher@prauscher.homeip.net>
2011-12-22 18:20:56 +0000

    general irc environment

---
+++ database.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class ExampleDatabase:
+	def __init__(self):
+		pass
+
+	def something(self, context, line):
+		context.reply('u mad?')
---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ircsession import IRCSession
+from database import ExampleDatabase
+
+irc = IRCSession('172.22.24.1', 6667, 'WGSklave', 'wgbot', 'Haushälter im Alfred-Messel-Wg 8a (WG 61)', None)
+
+database = ExampleDatabase()
+
+# TODO initialize database, register commands
+irc.registerCommand('!ping', lambda context, line:
+	context.reply(line))
+
+irc.registerCommand('!something', database.something)
+
+irc.join('#8a-61')
